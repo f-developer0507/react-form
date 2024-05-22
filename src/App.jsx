685a8319133216139c0b67789626ad34fa17e49f
f-developer0507 <stormpubg0507@gmail.com>
2024-05-22 05:49:15 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,26 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
   const [users, setUsers] = useState([]);
+  const [form, setForm] = useState({});
 
-  const addUser = () => {
-
-    users.push({name,age,phone,address})
-    setUsers([...users])
-    
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    users.push(form);
+    setUsers([...users]);
+    e.target.reset();
+  };
+
   return (
     <div className="container">
       <div className="row mt-5">
         <div className="col-md-8">
-          <table className="table table-bordered">
+          <table className="table table-bordered table-hover table-striped">
             <thead>
               <tr>
                 <td>T/R</td>
@@ -29,17 +32,15 @@ const App = () => {
               </tr>
             </thead>
             <tbody>
-              {
-                users.map((item,index)=>(
-                  <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{item.name}</td>
-                    <td>{item.age}</td>
-                    <td>{item.phone}</td>
-                    <td>{item.address}</td>
-                  </tr>
-                ))
-              }
+              {users?.map((item, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item.name}</td>
+                  <td>{item.age}</td>
+                  <td>{item.phone}</td>
+                  <td>{item.address}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
@@ -49,35 +50,43 @@ const App = () => {
               <h3 className="text-center">Add User</h3>
             </div>
             <div className="card-body">
-              <form>
+              <form id="submit" onSubmit={handleSubmit}>
                 <input
-                  onChange={(e) => setName(e.target.value)}
+                  name="name"
+                  onChange={handleChange}
                   type="text"
                   placeholder="Name"
                   className="form-control my-2"
+                  required
                 />
                 <input
-                  onChange={(e) => setAge(e.target.value)}
+                  name="age"
+                  onChange={handleChange}
                   type="number"
                   placeholder="Age"
                   className="form-control my-2"
+                  required
                 />
                 <input
-                  onChange={(e) => setPhone(e.target.value)}
+                  name="phone"
+                  onChange={handleChange}
                   type="tel"
                   placeholder="Phone"
                   className="form-control my-2"
+                  required
                 />
                 <input
-                  onChange={(e) => setAddress(e.target.value)}
+                  name="address"
+                  onChange={handleChange}
                   type="text"
                   placeholder="Address"
                   className="form-control my-2"
+                  required
                 />
               </form>
             </div>
             <div className="card-footer">
-              <button className="btn btn-primary" onClick={addUser}>
+              <button className="btn btn-primary" type="submit" form="submit">
                 add user
               </button>
             </div>
